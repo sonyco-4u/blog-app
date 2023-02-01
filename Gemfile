2c1d3bbea5f9e7b8e6e7b93a485cb25807fe0d08
Ezema Anthony Sunday <tonyunday@gmail.com>
2023-02-01 14:41:26 -0800

    Add controllers specs

--- Gemfile
+++ Gemfile
@@ -53,6 +53,7 @@ gem 'bootsnap', require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'rspec-rails', '~> 6.0.0'
 end
 
 group :development do
@@ -72,4 +73,6 @@ group :test do
   gem 'selenium-webdriver'
   gem 'webdrivers'
 end
-gem 'rspec-rails', '~> 6.0'
+# gem 'rspec-rails', '~> 6.0'
+
+gem 'rails-controller-testing', '~> 1.0'
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,28 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe "Users", type: :request do
-#   describe "GET /index" do
-#     pending "add some examples (or delete) #{__FILE__}"
-#   end
-# end
+RSpec.describe 'Users', type: :request do
+  describe 'GET /users' do
+    before(:example) { get users_path }
+    it 'the request is a success' do
+      expect(response).to have_http_status(200)
+    end
+    it 'Renders the right template' do
+      expect(response).to render_template('index')
+    end
+    it 'Include the correct text' do
+      expect(response.body).to include('List of all users')
+    end
+  end
+  describe 'GET /show' do
+    before(:example) { get user_path(id: 1) }
+    it 'the request is a success' do
+      expect(response).to have_http_status(200)
+    end
+    it 'Renders the right template' do
+      expect(response).to render_template('show')
+    end
+    it 'Include the correct text' do
+      expect(response.body).to include('Here is data and posts for certain user')
+    end
+  end
+end
